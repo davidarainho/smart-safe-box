9cee5f163e6110932645c157a51642e8d01b96f4
ManuelFSousa <manuelsousa2001@gmail.com>
2023-04-12 14:32:17 +0100

    Updates handle of REST requests

--- development/network/appConnectLayer.py
+++ development/network/appConnectLayer.py
@@ -1,59 +1,154 @@
 # app.py
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+users = []
 
-user = [
-    {"id": "User1", "username": "user1", "password": "pass1", "email": "email1", "active_locks": 3},
-    {"id": "User2", "username": "user2", "password": "pass2", "email": "email2", "active_locks": 3},
-    {"id": "User3", "username": "user3", "password": "pass3", "email": "email3", "active_locks": 3},
-]
+# -------------------------------------------------------------------------- Load Testing Structure -------------------------------------------------------------------------------
+f = open('testingStructure.json')
+users = json.load(f)
+f.close()
+# -------------------------------------------------------------------------- Load Testing Structure -------------------------------------------------------------------------------
 
-lock = [
-
-]
-
+# Opening Page
 @app.route('/')
 def hello_world():
-   return "Hello World"
+   return "Welcome to the REST based backend of your locker"
 
+# -------------------------------------------------------------------------- Get and Return Variables -------------------------------------------------------------------------------
+
+# Get an User Object - Args = username
+@app.get("/user")
+def get_user():
+    args = request.args
+
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            return jsonify(users[i])
+
+# Get User Object Username - Args = username
 @app.get("/user/username")
 def get_username():
     args = request.args
 
-    for i in range(len(user)):
-        if(user[i].get("id") == args.get("name", default = "", type = str)):
-            return jsonify(user[i].get("username"))
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            return jsonify(users[i].get("username"))
     
-
+# Get User Object Password - Args = username
 @app.get("/user/password")
 def get_password():
     args = request.args
 
-    for i in range(len(user)):
-        if(user[i].get("id") == args.get("name", default = "", type = str)):
-            return jsonify(user[i].get("password"))
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            return jsonify(users[i].get("password"))
 
+# Get User Object Email Address - Args = username
 @app.get("/user/email")
 def get_email():
     args = request.args
 
-    for i in range(len(user)):
-        if(user[i].get("id") == args.get("name", default = "", type = str)):
-            return jsonify(user[i].get("email"))
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            return jsonify(users[i].get("email"))
 
+# Get User Object Active Locks Array - Args = username
 @app.get("/user/active_locks")
-def get_active_lock():
+def get_active_lock_array():
     args = request.args
 
-    for i in range(len(user)):
-        if(user[i].get("id") == args.get("name", default = "", type = str)):
-            return jsonify(user[i].get("active_locks"))
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            return jsonify(users[i].get("active_locks"))
+        
+# Get User Object Specific Lcoker - Args = username, lockerID
+@app.get("/user/get_lock")
+def get_lock():
+    args = request.args
 
-@app.post("/user/username")
-def add_country():
-    
+    for i in range(len(users)):
+        if(users[i].get("username") == args.get("username", default = "", type = str)):
+            for j in range(len(users[i].get("active_locks"))):
+                if(users[i].get("active_locks")[j].get("lockID") == args.get("lockerID", default = "", type = str)):
+                    return jsonify(users[i].get("active_locks")[j])
+
+# -------------------------------------------------------------------------- Update and Create Variables ----------------------------------------------------------------------------
+
+# Add New User to the System
+@app.post("/user")
+def add_user():
+    if request.is_json:
+        newUser = request.get_json()
+        users.append(newUser)
+        return newUser, 201
     return {"error": "Request must be JSON"}, 415
+
+# Update Username of a Specific User - Args = oldUsername, newUsername
+@app.post("/user/username")
+def update_username():
+    args = request.args
+
+    if request.is_json:
+        newUsername = args.get("newUsername", default = "", type = str)
+        for i in range(len(users)):
+            if(users[i].get("username") == args.get("oldUsername", default = "", type = str)):
+                users[i].update({"username": newUsername})
+        return newUsername, 201
+    return {"error": "Request must be JSON"}, 415
+
+# Update Password of a Specific User - Args = username, oldPassword, newPassword
+@app.post("/user/password")
+def update_password():
+    args = request.args
+
+    if request.is_json:
+        newPassword = args.get("newPassword", default = "", type = str)
+        for i in range(len(users)):
+            if(users[i].get("password") == args.get("oldPassword", default = "", type = str)):
+                users[i].update({"username": newPassword})
+        return newPassword, 201
+    return {"error": "Request must be JSON"}, 415
+
+# Update Email Address of a Specific User - Args = username, oldEmail, newEmail
+@app.post("/user/email")
+def update_email():
+    args = request.args
+
+    if request.is_json:
+        newEmail = args.get("newEmail", default = "", type = str)
+        for i in range(len(users)):
+            if(users[i].get("email") == args.get("oldEmail", default = "", type = str)):
+                users[i].update({"username": newEmail})
+        return newEmail, 201
+    return {"error": "Request must be JSON"}, 415
+
+# Add Locker to User - Args = username, lock
+@app.post("/user/openLock")
+def addLock():
+    if request.is_json:
+        newLock = request.get_json()
+        for i in range(len(users)):
+            if(users[i].get("email") == args.get("oldEmail", default = "", type = str)):
+                users[i].get("active_locks").append(newLock)
+        return newLock, 201
+    return {"error": "Request must be JSON"}, 415
+
+# Open Locker - Args = username, lockID
+@app.post("/user/openLock")
+def openLock():
+    args = request.args
+
+    if request.is_json:
+        for i in range(len(users)):
+            if(users[i].get("username") == args.get("username", default = "", type = str)):
+                print("I just don't want errors")
+                # Call open SafeBox Function
+        return "open", 201
+    return {"error": "Request must be JSON"}, 415
+
+# --------------------------------------------------------------------------------- Main Function -----------------------------------------------------------------------------------
 
 if __name__ == '__main__':
    app.run(host = "0.0.0.0", debug = True)
